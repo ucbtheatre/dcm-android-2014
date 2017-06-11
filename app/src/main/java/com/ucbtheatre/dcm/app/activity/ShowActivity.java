7a42a57e55e1eaaa58720ce2a7289b6cf0762599
Kurt Guenther <kurtguenther@gmail.com>
2017-06-11 18:52:28 -0400

    DCM 18->19

--- app/src/main/java/com/ucbtheatre/dcm/app/activity/ShowActivity.java
+++ app/src/main/java/com/ucbtheatre/dcm/app/activity/ShowActivity.java
@@ -226,7 +226,7 @@ public class ShowActivity extends Activity {
 
     public String getShareString(Show show) {
         Performance perf = performances.get(0);
-        return String.format("Check out \"%s\" at %s, %s %s.  #DCM18 http://www.delclosemarathon.com/calendar", show.name, perf.venue.name, perf.getStartDay(), perf.getStartTime());
+        return String.format("Check out \"%s\" at %s, %s %s.  #DCM19 http://www.delclosemarathon.com/calendar", show.name, perf.venue.name, perf.getStartDay(), perf.getStartTime());
     }
 
     @Override
@@ -261,7 +261,7 @@ public class ShowActivity extends Activity {
         } else {
             Intent shareIntent = new Intent(Intent.ACTION_SEND);
             shareIntent.putExtra(Intent.EXTRA_TEXT, getShareString(show));
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Del Close Marathon 18"); //For a nice email
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Del Close Marathon 19"); //For a nice email
             shareIntent.setType("text/plain");
 
             startActivity(Intent.createChooser(shareIntent, "Share"));
