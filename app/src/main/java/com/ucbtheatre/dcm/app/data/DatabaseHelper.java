b19ce317d20cb0a7abcfe4e41c4491562651498e
Kurt Guenther <kurtguenther@gmail.com>
2018-06-14 17:24:44 -0400

    Bump database name to avoid conflicts with previous DB

--- app/src/main/java/com/ucbtheatre/dcm/app/data/DatabaseHelper.java
+++ app/src/main/java/com/ucbtheatre/dcm/app/data/DatabaseHelper.java
@@ -19,7 +19,7 @@ import java.sql.SQLException;
  */
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
-    private static final String DATABASE_NAME = "dcm.db";
+    private static final String DATABASE_NAME = "dcm20.db";
     private static final int DATABASE_VERSION = 23;
 
 
